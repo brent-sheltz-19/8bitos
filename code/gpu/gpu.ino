c46783ff0547c060d0db2a6d9f6b12646182a06d
brent-sheltz-19 <52083930+brent-sheltz-19@users.noreply.github.com>
2021-06-14 10:43:30 -0400

    gpu update
    switch to use of liquidcrystal_74hc595 library

--- code/gpu/gpu.ino
+++ code/gpu/gpu.ino
@@ -1,93 +1,115 @@
-#include <arduino.h>
-#include <LiquidCrystal.h>
-#include <avr/pgmspace.h>
+#include <LiquidCrystal_74HC595.h>
 
+#include <LiquidCrystal.h>
+// LiquidCrystal_74HC595(uint8_t ds, uint8_t shcp, uint8_t stcp,
 uint8_t rspin = 8;
 uint8_t rwpin = 9;
-uint8_t messegepin=10;
-const PROGMEM uint8_t pins[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-LiquidCrystal disp1 = LiquidCrystal(rspin, rwpin, 7, 3, 2, 1, pins[0]);
-LiquidCrystal disp2 = LiquidCrystal(rspin, rwpin, 6, 3, 2, 1, pins[0]);
-LiquidCrystal disp3 = LiquidCrystal(rspin, rwpin, 5, 3, 2, 1, pins[0]);
-LiquidCrystal disp4 = LiquidCrystal(rspin, rwpin, 4, 3, 2, 1, pins[0]);
-LiquidCrystal *disparry[4] = {&disp1, &disp2, &disp3, &disp4};
-char customchar[8] = {};
+uint8_t incommingmessage=10;
+const PROGMEM uint8_t pins[11]={0,1,2,3,4,5,6,7,8,9,10};
+LiquidCrystal_74HC595 disp1 = LiquidCrystal_74HC595(3,4,5,1, 3, 4, 5, 6, 7);
+LiquidCrystal_74HC595 disp2 = LiquidCrystal_74HC595(3,4,6,1, 3, 4, 5, 6, 7);
+LiquidCrystal_74HC595 disp3 = LiquidCrystal_74HC595(3,4,7,1, 3, 4, 5, 6, 7);
+LiquidCrystal_74HC595 disp4 = LiquidCrystal_74HC595(3,4,8,1, 3, 4, 5, 6, 7);
+LiquidCrystal_74HC595* disparry[4]={&disp1,&disp2,&disp3,&disp4};
+
+char customchar[8]
+{
+
+};
 
 int row, colum;
 
-struct message {
+struct message  
+{
   char instruction;
   char lengthofstr;
   String messagestr;
+
+
 };
-message mess = message();
-void writedisp(String out) {
-  for (char a : out) {
-    if (colum >= 64) {
-      colum = 0;
-      if (row == 0) {
+message mess=message();
+void writedisp(String out)
+{
+  for(char a :out)
+  {
+    if(colum>=64)
+    {
+      colum=0;
+      if(row==0)
+      {
 
-        disp1.setCursor(0, 1);
-        disp2.setCursor(0, 1);
-        disp3.setCursor(0, 1);
-        disp4.setCursor(0, 1);
-
-        row++;
+      
+      disp1.setCursor(0,1);
+      disp2.setCursor(0,1);
+      disp3.setCursor(0,1);
+      disp4.setCursor(0,1);
+      
+      row++;
       }
-	  else {
-        disp1.setCursor(0, 0);
-        disp2.setCursor(0, 0);
-        disp3.setCursor(0, 0);
-        disp4.setCursor(0, 0);
-        row = 0;
+      else
+      {
+        disp1.setCursor(0,0);
+        disp2.setCursor(0,0);
+        disp3.setCursor(0,0);
+        disp4.setCursor(0,0);
+        row=0;
       }
     }
-    if (colum >= 0 && colum < 16) {
-      disp4.noCursor();
-	  disp1.Cursor();
+    if(colum >=0 && colum<16)
+    {
       disp1.print(a);
-
-    } else if (colum >= 16 && colum < 32) {
-      disp1.noCursor();
-	  disp2.Cursor();
-	  disp2.print(a);
-    } else if (colum >= 32 && colum < 48) {
-      disp2.noCursor();
-	  disp3.Cursor();
-	  disp3.print(a);
-    } else if (colum >= 48 && colum < 64) {
-      disp3.noCursor();
-	  disp4.Cursor();
-	  disp4.print(a);
+      colum++;
     }
-    colum++;
+    else if(colum >=16 && colum<32)
+    {
+      disp1.print(a);
+      colum++;
+    }
+    else if(colum >=32 && colum<48)
+    {
+      disp1.print(a);
+      colum++;
+    }
+    else if(colum >=48 && colum<64)
+    {
+      disp1.print(a);
+      colum++;
+    }
   }
 }
-void readMessage(message alt) {
-  alt.instruction = shiftIn(0, 0, MSBFIRST);
-  alt.lengthofstr = shiftIn(0, 0, MSBFIRST);
-  int i = 0;
-  while (i < alt.lengthofstr) {
-    alt.messagestr.concat(shiftIn(0, 0, MSBFIRST));
+void readMessage(message alt)
+{
+  alt.instruction = shiftIn(0,0,MSBFIRST);
+  alt.lengthofstr = shiftIn(0,0,MSBFIRST);
+  int i =0;
+  while(i<alt.lengthofstr)
+  {
+    alt.messagestr.concat(shiftIn(0,0,MSBFIRST));
   }
+  
+
 }
 
 void setup() {
-  row = 0;
-  colum = 0;
-  for (uint8_t val : pins) {
-    pinMode(val, OUTPUT);
+  row, colum = 0;
+  for (uint8_t val:pins)
+  {
+    pinMode(val,OUTPUT);
   }
-  for (int i = 0; i < 4; i++) {
-    disparry[i]->begin(16, 2);
-    if (i == 0) {
-      disparry[0]->cursor();
-    } else {
-      disparry[i]->noCursor();
-    }
+  for(int i =0; i<4;i++)
+  {
+    disparry[i]->begin(16,2);
   }
+
 }
 void loop() 
 {
-	
+
+  if(digitalRead(incommingmessage)==HIGH)
+  {
+    
+  }
+
+
+
 }
