a05fb44b4322a0323a0a65a50ac4df2a249d8ed7
brent-sheltz-19 <52083930+brent-sheltz-19@users.noreply.github.com>
2024-12-15 02:32:32 -0500

    Update basiccompiler.py

--- code/toolchain/basiccompiler/basiccompiler.py
+++ code/toolchain/basiccompiler/basiccompiler.py
@@ -1,32 +1,32 @@
 import sys
-
 flags =[]
-filename:str
-additionalfiles = []
+filename:str = ''
+additionalfiles:str = []
 fileout = ""
 def claprocessor():
+    global filename
     setnext=0
     for stri in sys.argv:
         if stri.__contains__('.basic'):
-            if not filename:
-                filename =stri
-            else:
+            if filename:
                 additionalfiles.append(stri)
+            else:
+                filename =stri
         elif stri.__eq__("-o"):
             setnext = 2
         elif stri.__eq__("-h") or stri.__eq__('help'):
-            print("-o filename")
+            print("-o filename for output")
 
         if setnext == 2:
             fileout = stri
 
     return 0
 def main():
+    
 
-
-
+    return
 if __name__=="__main__":
     claprocessor()
-
+    main()
 
 
