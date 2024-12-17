a3e2c400586f1145709c255454ecc8148f681cbc
brent-sheltz-19 <52083930+brent-sheltz-19@users.noreply.github.com>
2024-12-17 02:44:02 -0500

    Update basiccompiler.py

--- code/toolchain/basiccompiler/basiccompiler.py
+++ code/toolchain/basiccompiler/basiccompiler.py
@@ -3,6 +3,9 @@ flags =[]
 filename:str = ''
 additionalfiles:str = []
 fileout = ""
+class instruction:
+    def __init__(self,line=''):
+        self.line =line
 def claprocessor():
     global filename
     setnext=0
@@ -15,11 +18,11 @@ def claprocessor():
         elif stri.__eq__("-o"):
             setnext = 2
         elif stri.__eq__("-h") or stri.__eq__('help'):
+            print("")
             print("-o filename for output")
-
+            
         if setnext == 2:
             fileout = stri
-
     return 0
 def main():
     
